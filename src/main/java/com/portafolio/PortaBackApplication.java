3ee1ae5a08e343d90c7dbd2cd5f35da55caf355a
CadelaPaciaroi <candela.paciaroni@hotmail.com>
2022-12-24 05:42:08 -0300

    Añadir archivo inicial

---
+++ src/main/java/com/portafolio/PortaBackApplication.java
@@ -0,0 +1,13 @@
+package com.portafolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortaBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortaBackApplication.class, args);
+	}
+
+}
